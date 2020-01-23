3003425291d160eeece0155878e8d6166216bc10
lynnettetee <43146875+lynnettetee@users.noreply.github.com>
2020-01-23 05:57:16 -0500

    Updates

---
+++ precache-manifest.fc2df3d5198f593588ad58110843069b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d2d709b4b273c408177f96e9dc695f",
+    "url": "/react-gamecheck/index.html"
+  },
+  {
+    "revision": "b252a3d53a2b18156e77",
+    "url": "/react-gamecheck/static/css/main.773ecfa9.chunk.css"
+  },
+  {
+    "revision": "7eb33e39a86db6970f81",
+    "url": "/react-gamecheck/static/js/2.d5fb4892.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-gamecheck/static/js/2.d5fb4892.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b252a3d53a2b18156e77",
+    "url": "/react-gamecheck/static/js/main.a5ba6db3.chunk.js"
+  },
+  {
+    "revision": "357947b7aaf83769144e",
+    "url": "/react-gamecheck/static/js/runtime-main.fb903481.js"
+  },
+  {
+    "revision": "ccf316a20d6a011bab9ce2d2bd50372d",
+    "url": "/react-gamecheck/static/media/Ps3_KingdomHearts.ccf316a2.jpg"
+  },
+  {
+    "revision": "a94854e4fa94886022f425f05e7d0b62",
+    "url": "/react-gamecheck/static/media/Ps4_DeathStranding.a94854e4.jpg"
+  },
+  {
+    "revision": "948e6b5de17404b5b63f0292230b0bea",
+    "url": "/react-gamecheck/static/media/Ps4_FinalFantasy15.948e6b5d.jpg"
+  },
+  {
+    "revision": "6612a8e571bb00485616245fe08e873c",
+    "url": "/react-gamecheck/static/media/Ps4_Persona5.6612a8e5.jpg"
+  },
+  {
+    "revision": "f8fcc1a9a4b7161bdc8f0f83ccdb620c",
+    "url": "/react-gamecheck/static/media/Switch_MarioKart8Deluxe.f8fcc1a9.jpg"
+  },
+  {
+    "revision": "49c32ec6e232111e35eb4ebbb0e202c5",
+    "url": "/react-gamecheck/static/media/bg.49c32ec6.jpg"
+  },
+  {
+    "revision": "cff7079d8d814262ffc38f81af4728af",
+    "url": "/react-gamecheck/static/media/gc.cff7079d.png"
+  },
+  {
+    "revision": "fe61b11d2bfc9bb497bd0cd78e5ff759",
+    "url": "/react-gamecheck/static/media/gradient-bg.fe61b11d.jpg"
+  },
+  {
+    "revision": "f6a8e96e1d8a4af0e1451af08847a828",
+    "url": "/react-gamecheck/static/media/p5-gp1.f6a8e96e.jpg"
+  },
+  {
+    "revision": "9692bd0918e0505ce3aff5b0e46638e2",
+    "url": "/react-gamecheck/static/media/p5-gp2.9692bd09.jpg"
+  },
+  {
+    "revision": "a506fcb349d59f888d45208c1d6f5175",
+    "url": "/react-gamecheck/static/media/p5-gp3.a506fcb3.jpg"
+  },
+  {
+    "revision": "1b83beb319b3e13d0762273ebde6abb7",
+    "url": "/react-gamecheck/static/media/teen-esrb-rating.1b83beb3.png"
+  }
+]);
